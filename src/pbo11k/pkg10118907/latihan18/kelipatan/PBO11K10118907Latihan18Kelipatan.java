8aeb0e97dd7fefde44b4be1d0a5b3e5a1872f60d
unknown <fadlullahyazid@gmail.com>
2019-10-08 23:26:18 +0700

    First Commit

---
+++ src/pbo11k/pkg10118907/latihan18/kelipatan/PBO11K10118907Latihan18Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan mengjitung kelipatan
+ * 
+ */
+public class PBO11K10118907Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double ulang =  3.5;
+        
+        while(ulang <= 35) {
+            System.out.print(ulang+"\n");
+            ulang += 3.5; 
+        }
+    }
+    
+}
